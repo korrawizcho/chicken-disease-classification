2fd418aa55690278c183dc3256a1eb73401b2eb0
tm-ong-chotayapa <ongc@thinkingmachin.es>
2023-06-08 16:27:30 +0700

    adding dvc.yaml file

--- main.py
+++ main.py
@@ -29,15 +29,15 @@ except Exception as e:
     raise e
 
 
-# try:
-#     logger.info(f"*******************")
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#     obj = ModelTrainingPipeline()
-#     obj.main()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
 try:
