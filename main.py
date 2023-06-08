c2b4cad3a8715518fd2fac88792e9f0c040b3689
tm-ong-chotayapa <ongc@thinkingmachin.es>
2023-06-08 16:04:00 +0700

    adding evaluation complete

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from src.cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
+
+
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -26,10 +29,21 @@ except Exception as e:
     raise e
 
 
+# try:
+#     logger.info(f"*******************")
+#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#     obj = ModelTrainingPipeline()
+#     obj.main()
+#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+
 try:
     logger.info(f"*******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = ModelTrainingPipeline()
+    obj = EvaluationPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
